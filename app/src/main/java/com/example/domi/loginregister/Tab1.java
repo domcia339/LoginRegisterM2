3e3bb8c78907674c1104ecc15f1198f0b6c1da1e
domcia339 <dominika.dziurkowska05@gmail.com>
2017-05-11 22:24:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/domi/loginregister/Tab1.java
@@ -0,0 +1,35 @@
+package com.example.domi.loginregister;
+
+/**
+ * Created by Domi on 10.05.2017.
+ */
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+public class Tab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+         return rootView;
+    }
+}
+
---
+++ app/src/main/java/com/example/domi/loginregister/Tab2.java
@@ -0,0 +1,21 @@
+package com.example.domi.loginregister;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Domi on 10.05.2017.
+ */
+
+public class Tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+        return rootView;
+    }
+}
